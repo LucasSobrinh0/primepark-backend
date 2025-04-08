c460a9bfea0ef450f1a94184f94b7636e1e3ca35
Lucas Sobrinho <150403906+LucasSobrinh0@users.noreply.github.com>
2025-04-08 18:29:32 -0400

    Update wsgi.py

--- core/core/wsgi.py
+++ core/core/wsgi.py
@@ -1,16 +1,3 @@
-"""
-WSGI config for core project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-<<<<<<< HEAD
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-=======
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
->>>>>>> 508fb6559e942b3801c1fb4cc9f5ed312879794c
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
